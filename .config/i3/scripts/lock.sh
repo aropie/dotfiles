a514e8a26fb48677b32bdc184907ae18544156aa
Adrian Rodriguez <aropie@hotmail.com>
2018-05-03 09:21:07 -0500

    Added lockscreen config

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
+
+# Take a screenshot:
 scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
+
+# Create a blur on the shot:
+mogrify -scale 10% -scale 1000% /tmp/screen.png 
+
+# Add the lock to the blurred image:
+[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite /tmp/screen.png
+
+# Pause music (mocp and mpd):
+# mocp -P
+# mpc pause
+
+# Lock it up!
+i3lock -e -i /tmp/screen.png
+
+# If still locked after 20 seconds, turn off screen.
+sleep 20 && pgrep i3lock && xset dpms force off
