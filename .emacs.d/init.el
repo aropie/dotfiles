78f0f376c65de4bf9115502f41ee468ad83c3d40
Adrian Rodriguez <aropie@hotmail.com>
2018-06-28 21:09:28 -0500

    Set Inconsolata as default font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,7 +13,6 @@
   (package-install 'spacemacs-theme))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,10 +21,7 @@
  '(custom-enabled-themes (quote (spacemacs-dark)))
  '(custom-safe-themes
    (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages
-   (quote
-    (markdown-mode adafruit-wisdom try magit diminish spaceline company rainbow-delimiters evil smex ido-vertical-mode org-bullets which-key use-package))))
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
