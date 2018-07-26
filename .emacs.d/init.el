5b65c4d1243c415fbc59d8af70949bd7e6b23e49
Adrian Rodriguez <aropie@hotmail.com>
2018-07-26 16:09:54 -0500

    Minor changes to spaceline config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,13 +18,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-begin-commands (quote (self-insert-command)))
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 2)
+ '(company-show-numbers t)
+ '(company-tooltip-align-annotations t)
  '(custom-enabled-themes (quote (spacemacs-dark)))
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(global-company-mode t)
  '(package-selected-packages
    (quote
-    (highlight-indent-guides smart-tabs-mode pdf-tools persp-mode markdown-mode adafruit-wisdom try magit diminish spaceline company rainbow-delimiters evil smex ido-vertical-mode org-bullets which-key use-package))))
+    (spaceline perspective highlight-indent-guides smart-tabs-mode pdf-tools persp-mode markdown-mode adafruit-wisdom try magit diminish company rainbow-delimiters evil smex ido-vertical-mode org-bullets which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
