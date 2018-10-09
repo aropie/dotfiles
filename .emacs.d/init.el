29837516dd78e848ee9cd44506fb0883e14fb9df
Adrian Rodriguez <aropie@hotmail.com>
2018-10-09 12:06:50 -0500

    Code cleaning

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,10 +8,6 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(unless (package-installed-p 'spacemacs-theme)
-  (package-refresh-contents)
-  (package-install 'spacemacs-theme))
-
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -30,12 +26,38 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(eyebrowse-new-workspace t t)
  '(eyebrowse-wrap-around t t)
+ '(fci-rule-color "#5B6268")
  '(global-company-mode t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(package-selected-packages
    (quote
-    (avy ## eyebrowse general spaceline highlight-indent-guides smart-tabs-mode pdf-tools persp-mode markdown-mode adafruit-wisdom try magit diminish company rainbow-delimiters evil smex ido-vertical-mode org-bullets which-key use-package)))
+    (projectile flx-ido avy ## eyebrowse general spaceline highlight-indent-guides smart-tabs-mode pdf-tools persp-mode markdown-mode adafruit-wisdom try magit diminish company rainbow-delimiters evil smex ido-vertical-mode org-bullets which-key use-package)))
  '(powerline-default-separator (quote wave))
- '(spaceline-highlight-face-func (quote spaceline-highlight-face-evil-state) t))
+ '(spaceline-highlight-face-func (quote spaceline-highlight-face-evil-state) t)
+ '(vc-annotate-background "#282c34")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#98be65")
+    (cons 40 "#b4be6c")
+    (cons 60 "#d0be73")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e6ab6a")
+    (cons 120 "#e09859")
+    (cons 140 "#da8548")
+    (cons 160 "#d38079")
+    (cons 180 "#cc7cab")
+    (cons 200 "#c678dd")
+    (cons 220 "#d974b7")
+    (cons 240 "#ec7091")
+    (cons 260 "#ff6c6b")
+    (cons 280 "#cf6162")
+    (cons 300 "#9f585a")
+    (cons 320 "#6f4e52")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
