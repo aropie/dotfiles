9d5fc0637bed3904b11a236708008960b51a3195
Adrian Rodriguez <aropie@hotmail.com>
2018-11-07 18:27:30 -0600

    Move .emacs to its own dir

---
+++ emacs/init.el
@@ -0,0 +1,11 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
