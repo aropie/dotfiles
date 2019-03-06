b9ee0fabbbe8dba692406ef31f3d31e0b9d944cd
Adrian Rodriguez <aropie@hotmail.com>
2019-03-06 13:19:53 -0600

    Fix spaceline breaking

--- emacs/init.el
+++ emacs/init.el
@@ -2,6 +2,8 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
