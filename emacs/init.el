efe0c1e721a324d0e55567c3796d680a3c8d5000
Adrian Rodriguez <aropie@hotmail.com>
2019-09-21 10:34:16 -0500

    Have a more clean emacs dir

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,11 @@
+;; Many modes store their cache files in =~/.emacs.d=. I prefer to keep
+;; those files in =~/.cache/emacs=
+(setq user-emacs-directory "~/.cache/emacs/")
+(if (not (file-directory-p user-emacs-directory))
+    (make-directory user-emacs-directory t))
+(setq auto-save-list-file-prefix user-emacs-directory)
+(setq package-user-dir "~/.cache/emacs/elpa")
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
