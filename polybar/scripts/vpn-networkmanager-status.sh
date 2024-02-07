69cb1fb9086e4d631259be86fae7e38b312c2ed0
Adrián Rodríguez <aropie@hotmail.com>
2024-02-07 15:38:33 -0600

    [POLYBAR] Slightly change string for VPN indication

--- polybar/scripts/vpn-networkmanager-status.sh
+++ polybar/scripts/vpn-networkmanager-status.sh
@@ -3,7 +3,7 @@
 vpn="$(nmcli -t -f name,type connection show --order name --active 2>/dev/null | grep vpn | head -1 | cut -d ':' -f 1)"
 
 if [ -n "$vpn" ]; then
-	echo "$vpn"
+	echo "VPN: $vpn"
 else
 	echo ""
-fi
+fi 
